0806a649e740968a85620c698306ba1d799e4591
FaizaanHy <fhyath@gmail.com>
2025-08-02 23:45:35 -0400

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,8 +1,27 @@
 <?php
-require 'db.php';
+session_start();
 
-session_unset();
+// Clear all session variables
+$_SESSION = [];
+
+// Destroy the session
 session_destroy();
 
+// Delete the session cookie by setting it in the past
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(
+        session_name(), 
+        '', 
+        time() - 42000,
+        $params["path"], 
+        $params["domain"],
+        $params["secure"], 
+        $params["httponly"]
+    );
+}
+
+// Redirect to homepage or login page
 header("Location: index2.php");
 exit;
+?>
